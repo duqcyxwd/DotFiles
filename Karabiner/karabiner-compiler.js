14752457234b3dc2f8390f852557d17f69686013
Yongqinchuan Du <duqcyxwd@gmail.com>
2020-03-26 15:09:48 -0400

    Update Amethyst config

--- Karabiner/karabiner-compiler.js
+++ Karabiner/karabiner-compiler.js
@@ -1,12 +1,12 @@
-console.log("Hi2"); // REMOVETAG
+console.log('Hi2'); // REMOVETAG
 
-const fs = require("fs");
+const fs = require('fs');
 
 var template = {
-  title: "",
+  title: '',
   rules: [
     {
-      description: "",
+      description: '',
       manipulators: []
     }
   ]
@@ -14,7 +14,7 @@ var template = {
 
 function populateInitManipulator(k, to, mode) {
   var initKey = {
-    type: "basic",
+    type: 'basic',
     from: {
       simultaneous: [
         {
@@ -25,8 +25,8 @@ function populateInitManipulator(k, to, mode) {
         }
       ],
       simultaneous_options: {
-        key_down_order: "strict",
-        key_up_order: "strict_inverse",
+        key_down_order: 'strict',
+        key_up_order: 'strict_inverse',
         to_after_key_up: [
           {
             set_variable: {
@@ -37,7 +37,7 @@ function populateInitManipulator(k, to, mode) {
         ]
       },
       modifiers: {
-        optional: ["any"]
+        optional: ['any']
       }
     },
     to: [
@@ -53,18 +53,18 @@ function populateInitManipulator(k, to, mode) {
       }
     ],
     parameters: {
-      "basic.simultaneous_threshold_milliseconds": 500
+      'basic.simultaneous_threshold_milliseconds': 500
     }
   };
   return initKey;
 }
 function populateManipulator(k, to, mode) {
   var kTemplate = {
-    type: "basic",
+    type: 'basic',
     from: {
       key_code: k,
       modifiers: {
-        optional: ["any"]
+        optional: ['any']
       }
     },
     to: [
@@ -75,7 +75,7 @@ function populateManipulator(k, to, mode) {
     ],
     conditions: [
       {
-        type: "variable_if",
+        type: 'variable_if',
         name: mode.mode,
         value: 1
       }
@@ -85,78 +85,106 @@ function populateManipulator(k, to, mode) {
 }
 
 var amethystMode = {
-  mode: "AmethystMode",
-  description: "Amethyst Mode All in one",
-  leadKey: "w",
+  mode: 'AmethystMode',
+  description: 'Amethyst Mode All in one',
+  leadKey: 'w',
   map: {
-    y: {
-      key_code: "y",
-      modifiers: ["left_control", "left_alt"],
-      note: "Change main window"
-    },
-    p: {
-      key_code: "p",
-      modifiers: ["left_control", "left_alt"]
-    },
-    j: {
-      key_code: "j",
-      modifiers: ["left_control", "left_alt"]
-    },
-    k: {
-      key_code: "k",
-      modifiers: ["left_control", "left_alt"]
-    },
-    h: {
-      key_code: "h",
-      modifiers: ["left_control", "left_alt"]
-    },
-    l: {
-      key_code: "l",
-      modifiers: ["left_control", "left_alt"]
+    c: {
+      key_code: 'c',
+      modifiers: ['left_alt', 'left_control']
     },
     f: {
-      key_code: "f",
-      modifiers: ["left_control", "left_alt"]
+      key_code: 'f',
+      modifiers: ['left_alt', 'left_control']
     },
-    t: {
-      key_code: "t",
-      modifiers: ["left_control", "left_alt"]
+    h: {
+      key_code: 'h',
+      modifiers: ['left_alt', 'left_control']
+    },
+    i: {
+      key_code: 'i',
+      modifiers: ['left_alt', 'left_control']
+    },
+    j: {
+      key_code: 'j',
+      modifiers: ['left_alt', 'left_control']
+    },
+    k: {
+      key_code: 'k',
+      modifiers: ['left_alt', 'left_control']
+    },
+    l: {
+      key_code: 'l',
+      modifiers: ['left_alt', 'left_control']
     },
     m: {
-      key_code: "m",
-      modifiers: ["left_control", "left_alt"]
+      key_code: 'm',
+      modifiers: ['left_alt', 'left_control']
     },
-    c: {
-      key_code: "c",
-      modifiers: ["left_control", "left_alt"]
+    o: {
+      key_code: 'o',
+      modifiers: ['left_alt', 'left_control']
+    },
+    p: {
+      key_code: 'p',
+      modifiers: ['left_alt', 'left_control']
+    },
+    t: {
+      key_code: 't',
+      modifiers: ['left_alt', 'left_control']
+    },
+    y: {
+      key_code: 'y',
+      modifiers: ['left_alt', 'left_control'],
+      note: 'Change main window'
     },
     spacebar: {
-      key_code: "spacebar",
-      modifiers: ["left_control", "left_alt"]
+      key_code: 'spacebar',
+      modifiers: ['left_control', 'left_alt']
     },
     return_or_enter: {
-      key_code: "return_or_enter",
-      modifiers: ["left_control", "left_alt"]
+      key_code: 'return_or_enter',
+      modifiers: ['left_control', 'left_alt']
     },
     left_arrow: {
-      key_code: "left_arrow",
-      modifiers: ["left_control", "left_alt"]
+      key_code: 'left_arrow',
+      modifiers: ['left_control', 'left_alt']
     },
     right_arrow: {
-      key_code: "right_arrow",
-      modifiers: ["left_control", "left_alt"]
+      key_code: 'right_arrow',
+      modifiers: ['left_control', 'left_alt']
     },
     comma: {
-      key_code: "comma",
-      modifiers: ["left_control", "left_alt"]
+      key_code: 'comma',
+      modifiers: ['left_control', 'left_alt']
     },
     period: {
-      key_code: "period",
-      modifiers: ["left_control", "left_alt"]
+      key_code: 'period',
+      modifiers: ['left_control', 'left_alt']
     },
     delete_or_backs: {
-      key_code: "delete_or_backs",
-      modifiers: ["left_control", "left_alt"]
+      key_code: 'delete_or_backs',
+      modifiers: ['left_control', 'left_alt']
+    },
+    open_bracket: {
+      key_code: 'open_bracket',
+      modifiers: ['left_control', 'left_alt']
+    },
+    close_bracket: {
+      key_code: 'close_bracket',
+      modifiers: ['left_control', 'left_alt']
+    },
+    backslash: {
+      key_code: 'backslash',
+      modifiers: ['left_control', 'left_alt']
+    },
+    equal_sign: {
+      key_code: 'equal_sign',
+      modifiers: ['left_control', 'left_alt']
+    },
+    hyphen: {
+      key_code: 'hyphen',
+      modifiers: ['left_control', 'left_alt']
     }
   }
 };
@@ -177,10 +205,10 @@ function populateConfig(mode) {
 
 var result = populateConfig(amethystMode);
 
-fs.writeFile("2019-10-18-Amethyst.json", result, err => {
+fs.writeFile('2020-03-Amethyst.json', result, err => {
   // throws an error, you could also catch it here
   if (err) throw err;
 
   // success case, the file was saved
-  console.log("File saved!");
+  console.log('File saved!');
 });
