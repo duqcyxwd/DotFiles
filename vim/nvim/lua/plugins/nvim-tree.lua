e18252ac6437cf61fe429d1cb9abd5defc047d6a
Yongqinchuan Du <duqcyxwd@gmail.com>
2022-10-21 10:14:43 -0400

    [vim] nvim-tree fix

--- vim/nvim/lua/plugins/nvim-tree.lua
+++ vim/nvim/lua/plugins/nvim-tree.lua
@@ -4,37 +4,16 @@ require("funcs.global")
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 SR'nvim-tree'.setup {
-  disable_netrw        = false,
-  hijack_netrw         = true,
-  open_on_setup        = false,
-  ignore_ft_on_setup   = {},
-  auto_close           = false,
-  auto_reload_on_write = true,
-  open_on_tab          = false,
-  hijack_cursor        = false,
-  update_cwd           = false,
-  hijack_unnamed_buffer_when_opening = false,
-  hijack_directories   = {
-    enable = true,
-    auto_open = true,
-  },
+  sort_by = "name",
+  open_on_setup = true,
+  open_on_setup_file = true,
   diagnostics = {
     enable = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    }
   },
   update_focused_file = {
-    enable      = false,
+    enable      = true,
     update_cwd  = false,
     ignore_list = {}
-  },
-  system_open = {
-    cmd  = nil,
-    args = {}
   },
   filters = {
     dotfiles = false,
@@ -46,23 +25,43 @@ SR'nvim-tree'.setup {
     timeout = 500,
   },
   view = {
-    width = 30,
-    height = 30,
     hide_root_folder = false,
-    side = 'left',
-    auto_resize = false,
-    preserve_window_proportions = false,
     mappings = {
       custom_only = false,
-      list = {}
+      list = {
+        { key = "?", action = "toggle_help" },
+
+      }
     },
-    number = false,
-    relativenumber = false,
-    signcolumn = "yes"
   },
-  trash = {
-    cmd = "trash",
-    require_confirm = true
+
+  renderer = {
+    highlight_git = true,
+    indent_markers = {
+      enable = true,
+    },
+    icons = {
+      padding = " ",
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
+      glyphs = {
+        git = {
+          unstaged = "",
+          staged = "",
+          unmerged = "",
+          renamed = "",
+          untracked = "",
+          deleted = "",
+          ignored = "",
+        },
+      },
+    },
+    special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+    symlink_destination = true,
   },
   actions = {
     change_dir = {
@@ -85,3 +84,5 @@ SR'nvim-tree'.setup {
     }
   }
 }
+
+DR("nvim-tree").setup()
